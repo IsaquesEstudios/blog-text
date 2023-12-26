f424abfd93ef35a140522436ccede6c3338b2d0f
Matteus Isaque <matteus.isaque28@gmail.com>
2023-12-26 09:40:01 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -2,16 +2,17 @@ if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `)
+  `);
 }
 
 const { protocol, hostname, port, pathname } = new URL(
   process.env.WORDPRESS_API_URL
-)
+);
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    domains: ["blog.isaquesestudios.com.br", "secure.gravatar.com"],
     remotePatterns: [
       {
         protocol: protocol.slice(0, -1),
@@ -21,4 +22,4 @@ module.exports = {
       },
     ],
   },
-}
+};
